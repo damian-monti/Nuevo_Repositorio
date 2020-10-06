7aa36c90bc550bd6b8d0ae4b5bb4f7d73237e3a2
damian-monti <mimaildamianmonti@gmail.com>
2020-10-06 15:04:48 -0300

    Función sumar persona

---
+++ app.js
@@ -0,0 +1,30 @@
+let personas = [
+    {
+        nombre: "Damián",
+        apellido: "Monti",
+    },
+    {
+        nombre: "Romina",
+        apellido: "Rios",
+    },
+    {
+        nombre: "Blanquito",
+        apellido: "Negro",
+    },
+    {
+        nombre: "Mona",
+        apellido: "Tricolor",
+    },
+]
+
+function sumarPersona (nombre, apellido) {
+    let personaNueva = {
+        nombre: nombre,
+        apellido: apellido,
+    };
+    personas.push(personaNueva);
+    console.log(personas)
+}
+
+
+sumarPersona("Juan", "Perez");
